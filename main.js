25b7dd0968034d19d65f13637c4d606eecc11481
simpsonyuki <yukilin0327@gmail.com>
2020-07-05 12:23:55 +0800

    innerHTML

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("我是JS!");
+// 先用console.log 測試檔案是否有連結成功(測試完可註解掉)
+// console.log("我是JS!");
+
+// 文件.使用ID取得元素("元素名稱").插入html="欲插入之內容"
+document.getElementById("box1").innerHTML="Hello~"
+// jQuery寫法: $("#box1").innerHTML="Hello~"
